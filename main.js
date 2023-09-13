077507a8a0200bffcb8b3a4cca414ed7958436fb
Mary Dean <mdean726@gmail.com>
2023-09-13 17:12:27 -0400

    Updated version using strings, +/- and & buttons do not work

--- main.js
+++ main.js
@@ -1,176 +1,187 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     alert("Hello!");
-// });
+document.addEventListener("DOMContentLoaded", function() {
 
-let calculation = [];
-let digitCount = 0;
-let indexCount = 0;
-let decimalIndicator = 0;
-let calced = 0;
+    let calculation = [];
+    let operatorSignal = 0;
+    let indexCount = 0;
+    let firstNum = "";
+    let secondNum = "";
 
-function pushNumber(number) {
-    if (digitCount === 0) {
-        calculation.push(number);
-        resultDisplay.innerHTML = number;
-    } else {
-        if (decimalIndicator === 0) {
-            let newNum = (calculation[indexCount] * 10) + number;
-            calculation.pop();
-            calculation.push(newNum);
-            resultDisplay.innerHTML = newNum;
+    function pushNumber(numberChar) {
+        if (operatorSignal === 0) {
+            firstNum = firstNum + numberChar;
+            calculation[indexCount] = firstNum;
+            resultDisplay.innerHTML = firstNum;
+            console.log(calculation);
+            return calculation;
+        };
+
+        secondNum = secondNum + numberChar;
+        calculation[indexCount] = secondNum;
+        resultDisplay.innerHTML = secondNum;
+        console.log(calculation);
+        return calculation;
+    };
+
+    function pushOperator(operator) {
+        calculation.push(operator);
+        indexCount = 2;
+        operatorSignal = 1;
+        console.log(calculation);
+        return calculation;
+    };
+
+    function clearArray() {
+        calculation = [];
+        operatorSignal = 0;
+        indexCount = 0;
+        firstNum = "";
+        secondNum = "";
+        resultDisplay.innerHTML = 0;
+        console.log(calculation);
+        return calculation;
+    };
+
+    function calculate(array) {
+        let num1 = parseFloat(firstNum);
+        let num2 = parseFloat(secondNum);
+        let answer = 0;
+
+        switch (array[1]) {
+            case "+":
+                answer = num1 + num2;
+                break;
+            case "-":
+                answer = num1 - num2;
+                break;
+            case "*":
+                answer = num1 * num2;
+                break;
+            case "/":
+                answer = num1 / num2;
+                break;
+        };
+        
+        let result;
+        if (Number.isInteger(answer)) {
+            result = answer;
         } else {
-            let numToAdd = number / (decimalIndicator * 10);
-            console.log(numToAdd);
-            calculation[indexCount] = calculation[indexCount] + numToAdd;
-            calculation[indexCount].toFixed(5);
-            console.log(calculation[indexCount]);
-            decimalIndicator = decimalIndicator * 10;
-            resultDisplay.innerHTML = calculation[indexCount];
-        }
-    }
-    digitCount++;
-    console.log(calculation);
-    return calculation;
-};
+            result = answer.toFixed(3);
+        };
 
-function pushOperator(operator) {
-    calculation.push(operator);
-    digitCount = 0;
-    indexCount = indexCount + 2;
-    decimalIndicator = 0;
-    console.log(calculation);
-    return calculation;
-};
+        calculation = [result.toString()];
+        firstNum = result;
+        secondNum = "";
+        indexCount = 1;
+        operatorSignal = 0;
+        console.log(calculation);
+        resultDisplay.innerHTML = result;
+        return calculation;
+    };
 
-function pushDecimal(decimal) {
-    let newStr = calculation[indexCount] + decimal;
-    let newFloat = parseFloat(newStr);
-    calculation.pop();
-    calculation.push(newFloat);
-    console.log(newFloat);
-    console.log(calculation);
-    resultDisplay.innerHTML = newFloat + ".";
-    decimalIndicator = 1;
-    return calculation; 
-}
+    // function changeSign() {
+    //     let num;
+    //     if (operatorSignal === 0) {
+    //         console.log("this works");
+    //         num = parseFloat(firstNum) * -1;
+    //         firstNum = num.toString();
+    //     } else {
+    //         console.log("this works");
+    //         num = parseFloat(secondNum) * -1;
+    //         secondNum = num.toString();
+    //     };
+    // };
 
-function clearArray() {
-    calculation = [];
-    digitCount = 0;
-    indexCount = 0;
-    decimalIndicator = 0;
-    resultDisplay.innerHTML = 0;
-    return calculation;
-}
+    const resultDisplay = document.querySelector("#result_display");
 
-function calculate(array) {
-    let answer = 0;
-    let i = 1;
-    if (array[i] === "+") {
-        answer = array[i-1] + array[i+1];
-    }
-    if (array[i] === "-") {
-        answer = array[i-1] - array[i+1];
-    }
-    if (array[i] === "*") {
-        answer = array[i-1] * array[i+1];
-    }
-    if (array[i] === "/") {
-        answer = array[i-1] / array[i+1];
-    }
-    calculation = [answer];
-    indexCount = 0;
-    digitCount = 1;
-    decimalIndicator = 0;
-    calced = 1;
-    console.log(calculation);
-    return answer;
-}
+    const number0 = document.querySelector("#number0");
+    number0.addEventListener("click", function() {
+        pushNumber("0");
+    });
 
-const number0 = document.querySelector("#number0");
-number0.addEventListener("click", function() {
-    pushNumber(0);
-});
+    const number1 = document.querySelector("#number1");
+    number1.addEventListener("click", function() {
+        pushNumber("1");
+    });
 
-const number1 = document.querySelector("#number1");
-number1.addEventListener("click", function() {
-    pushNumber(1);
-});
+    const number2 = document.querySelector("#number2");
+    number2.addEventListener("click", function() {
+        pushNumber("2");
+    });
 
-const number2 = document.querySelector("#number2");
-number2.addEventListener("click", function() {
-    pushNumber(2);
-});
+    const number3 = document.querySelector("#number3");
+    number3.addEventListener("click", function() {
+        pushNumber("3");
+    });
 
-const number3 = document.querySelector("#number3");
-number3.addEventListener("click", function() {
-    pushNumber(3);
-});
+    const number4 = document.querySelector("#number4");
+    number4.addEventListener("click", function() {
+        pushNumber("4");
+    });
 
-const number4 = document.querySelector("#number4");
-number4.addEventListener("click", function() {
-    pushNumber(4);
-});
+    const number5 = document.querySelector("#number5");
+    number5.addEventListener("click", function() {
+        pushNumber("5");
+    });
 
-const number5 = document.querySelector("#number5");
-number5.addEventListener("click", function() {
-    pushNumber(5);
-});
+    const number6 = document.querySelector("#number6");
+    number6.addEventListener("click", function() {
+        pushNumber("6");
+    });
 
-const number6 = document.querySelector("#number6");
-number6.addEventListener("click", function() {
-    pushNumber(6);
-});
+    const number7 = document.querySelector("#number7");
+    number7.addEventListener("click", function() {
+        pushNumber("7");
+    });
 
-const number7 = document.querySelector("#number7");
-number7.addEventListener("click", function() {
-    pushNumber(7);
-});
+    const number8 = document.querySelector("#number8");
+    number8.addEventListener("click", function() {
+        pushNumber("8");
+    });
 
-const number8 = document.querySelector("#number8");
-number8.addEventListener("click", function() {
-    pushNumber(8);
-});
+    const number9 = document.querySelector("#number9");
+    number9.addEventListener("click", function() {
+        pushNumber("9");
+    });
 
-const number9 = document.querySelector("#number9");
-number9.addEventListener("click", function() {
-    pushNumber(9);
-});
+    const period = document.querySelector("#period");
+    period.addEventListener("click", function() {
+        pushNumber(".");
+    });
 
-const period = document.querySelector("#period");
-period.addEventListener("click", function() {
-    pushDecimal(".");
-});
+    const clear = document.querySelector("#clear");
+    clear.addEventListener("click", function() {
+        clearArray();
+    });
 
-const clear = document.querySelector("#clear");
-clear.addEventListener("click", function() {
-    clearArray();
-});
+    const divide = document.querySelector("#divide");
+    divide.addEventListener("click", function() {
+        pushOperator("/");
+    });
 
-const divide = document.querySelector("#divide");
-divide.addEventListener("click", function() {
-    pushOperator("/");
-});
+    const multiply = document.querySelector("#multiply");
+    multiply.addEventListener("click", function() {
+        pushOperator("*");
+    });
 
-const multiply = document.querySelector("#multiply");
-multiply.addEventListener("click", function() {
-    pushOperator("*");
-});
+    const subtract = document.querySelector("#subtract");
+    subtract.addEventListener("click", function() {
+        pushOperator("-");
+    });
 
-const subtract = document.querySelector("#subtract");
-subtract.addEventListener("click", function() {
-    pushOperator("-");
-});
+    const add = document.querySelector("#add");
+    add.addEventListener("click", function() {
+        pushOperator("+");
+    });
 
-const add = document.querySelector("#add");
-add.addEventListener("click", function() {
-    pushOperator("+");
-});
+    const equals = document.querySelector("#equals");
+    equals.addEventListener("click", function() {
+        calculate(calculation);
+    });
 
-const resultDisplay = document.querySelector("#result_display");
-const equals = document.querySelector("#equals");
-equals.addEventListener("click", function() {
-    let calcedResult = calculate(calculation);
-    console.log(calcedResult);
-    resultDisplay.innerHTML = calcedResult.toFixed(3); 
+    const plusMinus = document.querySelector("#plus_minus");
+    plusMinus.addEventListener("click", function() {
+        console.log("clicked");
+        changeSign();
+    });
+
 });
